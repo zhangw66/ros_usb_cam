cd3c8204b7d63c5317810ef5c99048d295536d09
zhangwei <zhangwei@ainirobot.com>
2020-06-24 19:53:55 +0800

    节点匿名,可以preview多个摄像头

--- nodes/usb_cam_node.cpp
+++ nodes/usb_cam_node.cpp
@@ -268,7 +268,7 @@ public:
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "usb_cam");
+  ros::init(argc, argv, "", ros::init_options::AnonymousName);
   usb_cam::UsbCamNode a;
   a.spin();
   return EXIT_SUCCESS;
